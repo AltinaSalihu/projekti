1658ba15dc152485eb9412073e318698a8fc3d14
AltinaSalihu <75257807+AltinaSalihu@users.noreply.github.com>
2021-06-24 21:32:36 +0200

    Add files via upload

---
+++ src/dummyData.js
@@ -0,0 +1,291 @@
+export const userData = [
+    {
+      name: "Altina",
+      "Active User": 4000,
+    },
+    {
+      name: "Altina",
+      "Active User": 3000,
+    },
+    {
+      name: "Altina",
+      "Active User": 5000,
+    },
+    {
+      name: "Apr",
+      "Active User": 4000,
+    },
+    {
+      name: "May",
+      "Active User": 3000,
+    },
+    {
+      name: "Jun",
+      "Active User": 2000,
+    },
+    {
+      name: "Jul",
+      "Active User": 4000,
+    },
+    {
+      name: "Agu",
+      "Active User": 3000,
+    },
+    {
+      name: "Sep",
+      "Active User": 4000,
+    },
+    {
+      name: "Oct",
+      "Active User": 1000,
+    },
+    {
+      name: "Nov",
+      "Active User": 4000,
+    },
+    {
+      name: "Dec",
+      "Active User": 3000,
+    },
+  ];
+
+  export const productData = [
+    {
+      name: "Jan",
+      "Sales": 4000,
+    },
+    {
+      name: "Feb",
+      "Sales": 3000,
+    },
+    {
+      name: "Mar",
+      "Sales": 5000,
+    },
+  ];
+
+  export const userRows = [
+    {
+      id: 1,
+      username: "Altina",
+      avatar:
+        "https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      dataregjistrimie: "11-06-2021",
+      numripersonal: "10000002200",
+      emri: "Altina",
+      emriprindit: "Filan",
+      mbiemri:"Salihu",
+      drejtimi:"SHKI",
+
+    },
+    {
+      id: 2,
+      username: "Altina",
+      avatar:
+        "https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      dataregjistrimie: "11-06-2021",
+      numripersonal: "10000002200",
+      emri: "Altina",
+      emriprindit: "Filan",
+      mbiemri:"Salihu",
+      drejtimi:"SHKI",
+
+    },
+    {
+      id: 3,
+      username: "Altina",
+      avatar:
+        "https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      dataregjistrimie: "11-06-2021",
+      numripersonal: "10000002200",
+      emri: "Altina",
+      emriprindit: "Filan",
+      mbiemri:"Salihu",
+      drejtimi:"SHKI",
+
+    },
+    {
+      id: 4,
+      username: "Altina",
+      avatar:
+        "https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      dataregjistrimie: "11-06-2021",
+      numripersonal: "10000002200",
+      emri: "Altina",
+      emriprindit: "Filan",
+      mbiemri:"Salihu",
+      drejtimi:"SHKI",
+
+    },
+    {
+      id: 5,
+      username: "Altina",
+      avatar:
+        "https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      dataregjistrimie: "11-06-2021",
+      numripersonal: "10000002200",
+      emri: "Altina",
+      emriprindit: "Filan",
+      mbiemri:"Salihu",
+      drejtimi:"SHKI",
+
+    },
+    {
+      id: 6,
+      username: "Altina",
+      avatar:
+        "https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      dataregjistrimie: "11-06-2021",
+      numripersonal: "10000002200",
+      emri: "Altina",
+      emriprindit: "Filan",
+      mbiemri:"Salihu",
+      drejtimi:"SHKI",
+
+    },
+    {
+      id: 7,
+      username: "Altina",
+      avatar:
+        "https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      dataregjistrimie: "11-06-2021",
+      numripersonal: "10000002200",
+      emri: "Altina",
+      emriprindit: "Filan",
+      mbiemri:"Salihu",
+      drejtimi:"SHKI",
+
+    },
+    {
+      id: 8,
+      username: "Altina",
+      avatar:
+        "https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      dataregjistrimie: "11-06-2021",
+      numripersonal: "10000002200",
+      emri: "Altina",
+      emriprindit: "Filan",
+      mbiemri:"Salihu",
+      drejtimi:"SHKI",
+
+    },
+    {
+      id: 9,
+      username: "Altina",
+      avatar:
+        "https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      dataregjistrimie: "11-06-2021",
+      numripersonal: "10000002200",
+      emri: "Altina",
+      emriprindit: "Filan",
+      mbiemri:"Salihu",
+      drejtimi:"SHKI",
+
+    },
+    {
+      id: 10,
+      username: "Altina",
+      avatar:
+        "https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      dataregjistrimie: "11-06-2021",
+      numripersonal: "10000002200",
+      emri: "Altina",
+      emriprindit: "Filan",
+      mbiemri:"Salihu",
+      drejtimi:"SHKI",
+
+    },
+  ];
+
+  export const productRows = [
+    {
+      id: 1,
+      name: "Apple Airpods",
+      img:
+        "https://images.pexels.com/photos/7156886/pexels-photo-7156886.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      stock: 123,
+      status: "active",
+      price: "$120.00",
+    },
+    {
+      id: 2,
+      name: "Apple Airpods",
+      img:
+        "https://images.pexels.com/photos/7156886/pexels-photo-7156886.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      stock: 123,
+      status: "active",
+      price: "$120.00",
+    },
+    {
+      id: 3,
+      name: "Apple Airpods",
+      img:
+        "https://images.pexels.com/photos/7156886/pexels-photo-7156886.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      stock: 123,
+      status: "active",
+      price: "$120.00",
+    },
+    {
+      id: 4,
+      name: "Apple Airpods",
+      img:
+        "https://images.pexels.com/photos/7156886/pexels-photo-7156886.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      stock: 123,
+      status: "active",
+      price: "$120.00",
+    },
+    {
+      id: 5,
+      name: "Apple Airpods",
+      img:
+        "https://images.pexels.com/photos/7156886/pexels-photo-7156886.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      stock: 123,
+      status: "active",
+      price: "$120.00",
+    },
+    {
+      id: 6,
+      name: "Apple Airpods",
+      img:
+        "https://images.pexels.com/photos/7156886/pexels-photo-7156886.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      stock: 123,
+      status: "active",
+      price: "$120.00",
+    },
+    {
+      id: 7,
+      name: "Apple Airpods",
+      img:
+        "https://images.pexels.com/photos/7156886/pexels-photo-7156886.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      stock: 123,
+      status: "active",
+      price: "$120.00",
+    },
+    {
+      id: 8,
+      name: "Apple Airpods",
+      img:
+        "https://images.pexels.com/photos/7156886/pexels-photo-7156886.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      stock: 123,
+      status: "active",
+      price: "$120.00",
+    },
+    {
+      id: 9,
+      name: "Apple Airpods",
+      img:
+        "https://images.pexels.com/photos/7156886/pexels-photo-7156886.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      stock: 123,
+      status: "active",
+      price: "$120.00",
+    },
+    {
+      id: 10,
+      name: "Apple Airpods",
+      img:
+        "https://images.pexels.com/photos/7156886/pexels-photo-7156886.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      stock: 123,
+      status: "active",
+      price: "$120.00",
+    },
+  ];
