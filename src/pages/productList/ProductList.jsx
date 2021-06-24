40e18a9498f1dcc875a79ba1aa5d86a1aeb5f084
AltinaSalihu <75257807+AltinaSalihu@users.noreply.github.com>
2021-06-24 21:51:21 +0200

    Add files via upload

---
+++ src/pages/productList/ProductList.jsx
@@ -0,0 +1,67 @@
+import "./productList.css";
+import { DataGrid } from "@material-ui/data-grid";
+import { productRows } from "../../dummyData";
+import { useState } from "react";
+
+export default function ProductList() {
+  const [data, setData] = useState(productRows);
+
+ 
+
+  const columns = [
+    { field: "id", headerName: "ID", width: 90 },
+    {
+      field: "product",
+      headerName: "Product",
+      width: 200,
+      renderCell: (params) => {
+        return (
+          <div className="productListItem">
+            <img className="productListImg" src={params.row.img} alt="" />
+            {params.row.name}
+          </div>
+        );
+      },
+    },
+    { field: "stock", headerName: "Stock", width: 200 },
+    {
+      field: "status",
+      headerName: "Status",
+      width: 120,
+    },
+    {
+      field: "price",
+      headerName: "Price",
+      width: 160,
+    },
+    {
+      field: "action",
+      headerName: "Action",
+      width: 150,
+      renderCell: (params) => {
+        return (
+          <>
+            
+            
+          </>
+        );
+      },
+    },
+  ];
+
+  return (
+    <div className="productList">
+      <DataGrid
+        rows={data}
+        disableSelectionOnClick
+        columns={columns}
+        pageSize={8}
+        checkboxSelection
+      />
+    </div>
+    
+    
+  );
+}
+
+
